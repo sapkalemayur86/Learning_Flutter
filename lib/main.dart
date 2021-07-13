4c9b8905959e3c46ede6e2b6ccf9df9b0006d529
sapkalemayur86 <61945717+sapkalemayur86@users.noreply.github.com>
2021-07-13 13:15:31 +0530

    HomePage cretaed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/homepage.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,67 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Container(
-          color: Colors.blueGrey,
-          child: Column(
-            children: [
-              CircleAvatar(
-                backgroundImage: AssetImage('lib/assets/photo.jpg'),
-                radius: 100,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(20),
-                child: Text(
-                  'Mayur Sapkale',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 40.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                    fontFamily: 'myfont',
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(20),
-                child: Text(
-                  'Software Developer',
-                  style: TextStyle(
-                      fontSize: 20, color: Colors.white, letterSpacing: 2.0),
-                ),
-              ),
-              Card(
-                color: Colors.white,
-                child: ListTile(
-                  leading: Icon(
-                    Icons.phone,
-                    color: Colors.blueAccent,
-                  ),
-                  title: Text(
-                    '+91 73 78 732 199',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ),
-              Card(
-                color: Colors.white,
-                child: ListTile(
-                  leading: Icon(
-                    Icons.mail,
-                    color: Colors.blueAccent,
-                  ),
-                  title: Text(
-                    'sapkalemayur86@gmail.com',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                ),
-              )
-            ],
-          ),
-          padding: EdgeInsets.all(50),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
