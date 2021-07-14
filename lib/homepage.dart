f4341b193579281a127299cafb38481f418ea55d
sapkalemayur86 <61945717+sapkalemayur86@users.noreply.github.com>
2021-07-14 13:31:59 +0530

    intent changes performed

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HomePage extends StatelessWidget {
+  void customLaunch(command) async {
+    if (await canLaunch(command)) {
+      await launch(command);
+    } else {
+      print('could not luanch $command');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -8,64 +17,77 @@ class HomePage extends StatelessWidget {
         appBar: AppBar(
           title: Text("Portfolio"),
         ),
-        body: Container(
-          color: Colors.blueGrey,
-          child: Column(
-            children: [
-              CircleAvatar(
-                backgroundImage: AssetImage('lib/assets/photo.jpg'),
-                radius: 100,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(20),
-                child: Text(
-                  'Mayur Sapkale',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 40.0,
-                    fontWeight: FontWeight.bold,
+        body: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Container(
+            color: Colors.blueGrey,
+            child: Column(
+              children: [
+                CircleAvatar(
+                  backgroundImage: AssetImage('lib/assets/photo.jpg'),
+                  radius: 100,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Text(
+                    'Mayur Sapkale',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontFamily: 'myfont',
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Text(
+                    'Software Developer',
+                    style: TextStyle(
+                        fontSize: 20, color: Colors.white, letterSpacing: 2.0),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {
+                    customLaunch('tel: 7378732199');
+                  },
+                  child: Card(
                     color: Colors.white,
-                    fontFamily: 'myfont',
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.phone,
+                        color: Colors.blueAccent,
+                      ),
+                      title: Text(
+                        '+91 73 78 732 199',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(20),
-                child: Text(
-                  'Software Developer',
-                  style: TextStyle(
-                      fontSize: 20, color: Colors.white, letterSpacing: 2.0),
-                ),
-              ),
-              Card(
-                color: Colors.white,
-                child: ListTile(
-                  leading: Icon(
-                    Icons.phone,
-                    color: Colors.blueAccent,
+                InkWell(
+                  onTap: () {
+                    customLaunch('mailto:sapkalemayur86@gmail.com');
+                  },
+                  child: Card(
+                    color: Colors.white,
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.mail,
+                        color: Colors.blueAccent,
+                      ),
+                      title: Text(
+                        'sapkalemayur86@gmail.com',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ),
                   ),
-                  title: Text(
-                    '+91 73 78 732 199',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ),
-              Card(
-                color: Colors.white,
-                child: ListTile(
-                  leading: Icon(
-                    Icons.mail,
-                    color: Colors.blueAccent,
-                  ),
-                  title: Text(
-                    'sapkalemayur86@gmail.com',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                ),
-              )
-            ],
+                )
+              ],
+            ),
+            padding: EdgeInsets.all(50),
           ),
-          padding: EdgeInsets.all(50),
         ),
         drawer: Drawer(),
       ),
